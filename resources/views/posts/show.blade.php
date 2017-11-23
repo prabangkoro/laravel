6be9c7bdf2dd78ae1d3229d315716f48703ed990
babang <bagus.prabangkoro@xtremax.com>
2017-11-23 15:42:46 +0700

    [EDIT DELETE POST] add edit and delete post features

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,12 @@
         <h3>{{$post->title}}</h3>
         <small>Created at {{$post->created_at}}</small>
         <p>{!! $post->body !!}</p>
-        <a href="/posts" class="btn btn-primary btn-lg">Return</a>
+        <a href="/posts" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-menu-left"></span> All Post</a>
+        <a href="/posts/{{ $post->id }}/edit" class="btn btn-success btn-sm">Edit</a>
+
+        {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'class' => 'pull-right']) !!}
+            {!! Form::hidden('_method', 'DELETE') !!}
+            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+        {!! Form::close() !!}
     </div>
 @endsection
